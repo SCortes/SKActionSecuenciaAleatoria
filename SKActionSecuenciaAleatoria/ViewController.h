583c533d522c5db6e82a62e9e4c6c3b373de78a5
Sergio <sergio@MacBook-Pro-de-Sergio.local>
2014-02-20 23:18:12 +0100

    Inicio de repositorio SKActionSecuenciaAleatoria

---
+++ SKActionSecuenciaAleatoria/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SKActionSecuenciaAleatoria
+//
+
+//  Copyright (c) 2014 Sergio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
