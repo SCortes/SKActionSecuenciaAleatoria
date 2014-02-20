063d5b40029fc7214896504e0778be7dfa5b19b7
Sergio <sergio@MacBook-Pro-de-Sergio.local>
2014-02-21 00:27:16 +0100

    Añado una secuencia y genero la accion de los botones escalandose dependiendo de la secuencia generada

--- SKActionSecuenciaAleatoria/MyScene.m
+++ SKActionSecuenciaAleatoria/MyScene.m
@@ -14,7 +14,47 @@
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
+        SKAction *escalar = [SKAction sequence:@[
+                                                 [SKAction scaleTo:2 duration:0.5],
+                                                 [SKAction scaleTo:1 duration:0.5]
+                                                 ]];
+        SKAction *parada = [SKAction waitForDuration:1];
         
+        NSArray *secuencia = [[NSArray alloc] initWithObjects:@"1",@"2",@"3",@"4", nil];
+        
+        NSMutableArray * animaciones = [[NSMutableArray alloc] initWithCapacity:4];
+        
+        SKSpriteNode * boton1 = [[SKSpriteNode alloc] initWithImageNamed:@"parteRoja.png"];
+        boton1.position = CGPointMake(CGRectGetMidX(self.frame)-50, CGRectGetMidY(self.frame)+50);
+        boton1.name = @"1";
+        
+        SKSpriteNode * boton2 = [[SKSpriteNode alloc] initWithImageNamed:@"parteAzul.png"];
+        boton2.position = CGPointMake(CGRectGetMidX(self.frame)+50, CGRectGetMidY(self.frame)+50);
+        boton2.name = @"2";
+        
+        SKSpriteNode * boton3 = [[SKSpriteNode alloc] initWithImageNamed:@"parteMorada.png"];
+        boton3.position = CGPointMake(CGRectGetMidX(self.frame)-50, CGRectGetMidY(self.frame)-50);
+        boton3.name = @"3";
+        
+        SKSpriteNode * boton4 = [[SKSpriteNode alloc] initWithImageNamed:@"parteVerde.png"];
+        boton4.position = CGPointMake(CGRectGetMidX(self.frame)+50, CGRectGetMidY(self.frame)-50);
+        boton4.name = @"4";
+
+        [self addChild:boton1];
+        [self addChild:boton2];
+        [self addChild:boton3];
+        [self addChild:boton4];
+        
+        for(NSString * boton in secuencia){
+            SKAction * animacion = [SKAction runAction:escalar onChildWithName:boton];
+            
+            [animaciones addObject:animacion];
+            [animaciones addObject:parada];
+        }
+        
+        SKAction *ejecutaTodo = [SKAction sequence:animaciones];
+        
+        [self runAction:ejecutaTodo];
         
         
     }
